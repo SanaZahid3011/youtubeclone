521346646bc4f55615406e0a603bf3455852c502
sanaZahid3011 <zahid8488484884@gmail.com>
2025-05-28 18:39:42 +0500

    creating sidebar

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Sidebar from '../../Components/Sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Sidebar/>
+    </div>
   )
 }
 

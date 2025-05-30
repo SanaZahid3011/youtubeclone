9ca83037a0caee2d1a248c2f0996dcfff9b5eccc
sanaZahid3011 <zahid8488484884@gmail.com>
2025-05-30 18:52:50 +0500

    Adding the styling to the Feed Component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Feed from '../../Components/Feed/Feed'
+import '../../Style/Home.css';
 
 const Home = ({sidebar}) => {
   return (
     <div>
       <Sidebar sidebar={sidebar}/>
-      <div className={`container ${sidebar?" ":"large-container"}`}>
+      <div className={`container ${sidebar?" ":'large-container'}`}>
       <Feed/>
       </div>
     </div>

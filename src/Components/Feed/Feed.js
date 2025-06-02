1a67250ff34216179a2d5e844f97266c4fbab229
sanaZahid3011 <zahid8488484884@gmail.com>
2025-06-02 19:37:19 +0500

    Integrate moment package for date formatting

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,16 +1,10 @@
 import React,{useState,useEffect}from 'react'
 import '../../Style/Feed.css';
-import thumbnail1 from '../../assets/thumbnail1.png';
-import thumbnail2 from '../../assets/thumbnail2.png';
-import thumbnail3 from '../../assets/thumbnail3.png';
-import thumbnail4 from '../../assets/thumbnail4.png';
-import thumbnail5 from '../../assets/thumbnail5.png';
-import thumbnail6 from '../../assets/thumbnail6.png';
-import thumbnail7 from '../../assets/thumbnail7.png';
-import thumbnail8 from '../../assets/thumbnail8.png';
 import { Link } from 'react-router-dom';
 import {API_KEY} from '../../data';
 import {Value_Converter} from '../../data';
+import moment from 'moment';
+
 
 const Feed = ({category}) => {
   const [data ,setdata]= useState([])
@@ -31,7 +25,7 @@ const Feed = ({category}) => {
         <img src={item.snippet.thumbnails.medium.url} alt="" />
         <h2>{item.snippet.title}</h2>
         <h3>{item.snippet.channelTitle}</h3>
-        <p style={{ color:"#5a5a5a" }}>{ Value_Converter(item.statistics.viewCount)} views &bull; 2days ago</p>
+        <p style={{ color:"#5a5a5a" }}>{ Value_Converter(item.statistics.viewCount)} views &bull; {moment(item.snippet.publishedAt).fromNow()}</p>
     </Link>
       )
     })}
